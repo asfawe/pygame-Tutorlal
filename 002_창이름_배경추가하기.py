59ecf36e0c0df86faab73104164af9f3e40781cb
unknown <aronia5137@gmail.com>
2021-08-31 18:47:59 +0900

    first commit

---
+++ 002_창이름_배경추가하기.py
@@ -0,0 +1,20 @@
+import pygame as pg
+pg.init()
+
+화면가로길이 = 600
+화면세로길이 = 800
+
+화면 = pg.display.set_mode((화면가로길이,화면세로길이)) # 화면 백그라운드 설정
+
+pg.display.set_caption('생선잡기_게임') #게임 타이틀 이름 지정
+
+배경이미지 = pg.image.load("img/배경.png")
+배경이미지 = pg.transform.scale(배경이미지, (화면가로길이,화면세로길이))
+
+화면.blit(배경이미지, (0,0))
+pg.display.update()
+
+while True:
+    for 이벤트 in pg.event.get():
+        if 이벤트.type == pg.QUIT:
+            quit()
